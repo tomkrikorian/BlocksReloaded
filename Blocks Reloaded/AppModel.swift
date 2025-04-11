b793df13dba5c175519fd7fea1fc3c78905a838c
Tom Krikorian <dev@tom.vision>
2025-04-11 15:15:50 +0200

    Initial Commit

---
+++ Blocks Reloaded/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Blocks Reloaded
+//
+//  Created by Tom Krikorian on 11/04/2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Blocks Reloaded/Blocks_ReloadedApp.swift
@@ -0,0 +1,34 @@
+//
+//  Blocks_ReloadedApp.swift
+//  Blocks Reloaded
+//
+//  Created by Tom Krikorian on 11/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Blocks_ReloadedApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
